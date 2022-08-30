e7426eb76582541b5b992d896aa88a52ab8e216d
larimar <larimarfondeur@gmail.com>
2022-08-30 16:24:41 -0400

    commit tree 89195863267b52d0de08d93f1cbf3d1c51171a40

--- methods.rb
+++ methods.rb
@@ -1,1 +1,18 @@
 # Your code here!
+def my_method(param)
+    puts 'Running my_method'
+    param + 1 
+end 
+
+
+def greet_programmer()
+    puts 'Hello, programmer!}'
+end 
+
+def greet(Naureen) 
+    puts 'Hello, #{Naureen}!'
+    end
+{greet('Naureen')}
+
+
+
